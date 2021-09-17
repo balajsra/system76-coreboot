809eb8d9e19c3db0fad7e5af1583b2cecd8cfd64
Tim Crawford <tcrawford@system76.com>
2021-09-17 11:43:03 -0600

    mb/system76/*: Enable dGPUs
    Change-Id: Ib5bab02801407c8bf05e6028bf8f9fa7ccc5ecd0
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/mainboard/system76/addw1/variants/addw1/include/variant/dgpu.h
+++ src/mainboard/system76/addw1/variants/addw1/include/variant/dgpu.h
@@ -1,17 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#ifndef MAINBOARD_GPIO_H
-#define MAINBOARD_GPIO_H
+#ifndef VARIANT_DGPU_H
+#define VARIANT_DGPU_H
 
 #include <soc/gpio.h>
 
 #define DGPU_RST_N	GPP_F22
 #define DGPU_PWR_EN	GPP_F23
 #define DGPU_GC6	GPP_C12
-
-#ifndef __ACPI__
-void mainboard_configure_early_gpios(void);
-void mainboard_configure_gpios(void);
-#endif
 
 #endif
