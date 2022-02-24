381a2eadeb955d430fae7a797b3de378bee686fb
Jeremy Soller <jackpot51@gmail.com>
2022-02-24 16:21:06 -0700

    Add sunrise
    Change-Id: I4ae434552b2fa00a56e4e1ec4ec9360c9e82ee9b

---
+++ src/mainboard/system76/sunrise/early_gpio.c
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <soc/gpio.h>
+#include "gpio.h"
+
+/* GPIO pins used by coreboot should be initialized in bootblock */
+
+static const struct soc_amd_gpio gpio_set_stage_reset[] = {
+	//TODO
+};
+
+void mainboard_program_early_gpios(void)
+{
+	gpio_configure_pads(gpio_set_stage_reset, ARRAY_SIZE(gpio_set_stage_reset));
+}
